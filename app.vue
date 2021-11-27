4f7ae45ce1fec61cd881d3402652b33de6d7eaad
Kalane <kalanehd@gmail.com>
2021-11-28 00:06:03 +0100

    feat: start with nuxt3

---
+++ app.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import "./assets/tailwind.css";
+import "./assets/custom.css";
+
+console.log("loaded");
+
+/*
+// Back-to-top
+const backToTop = document.querySelector(".back-to-top");
+window.addEventListener("scroll", function () {
+  const scrollBarPosition = window.pageYOffset;
+  if (scrollBarPosition > 500) {
+    changeElementStates(backToTop, "opacity-100", "opacity-0");
+    changeElementStates(backToTop, "cursor-pointer", "cursor-default");
+  } else {
+    changeElementStates(backToTop, "opacity-0", "opacity-100");
+    changeElementStates(backToTop, "cursor-default", "cursor-pointer");
+  }
+});
+
+backToTop.addEventListener("click", function () {
+  const scrollBarPosition = window.pageYOffset;
+  if (scrollBarPosition > 500) {
+    document.documentElement.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest",
+    });
+  }
+});
+
+function changeElementStates(e, toAdd, toRemove) {
+  if (toAdd !== "") e.classList.add(toAdd);
+  if (toRemove !== "") e.classList.remove(toRemove);
+}
+*/
+</script>
