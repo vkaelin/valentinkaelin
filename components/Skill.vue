c9c63bc04c780d2f8361e4d3e37c2e81523f338f
Kalane <kalanehd@gmail.com>
2021-11-28 19:22:50 +0100

    refactor: create Skill component

---
+++ components/Skill.vue
@@ -0,0 +1,26 @@
+<template>
+  <a
+    :href="link"
+    :class="id"
+    class="skill flex items-center justify-center p-4 mb-1 mr-1 rounded-full"
+    target="_blank"
+  >
+    <span class="sr-only">{{ name }}</span>
+    <svg :viewBox="viewBox" class="dark:text-indigo-400 w-10 h-10 text-indigo-500 fill-current">
+      <slot />
+    </svg>
+  </a>
+</template>
+
+<script setup lang="ts">
+
+const { id, name, link } = defineProps({
+  id: String,
+  name: String,
+  link: String,
+  viewBox: {
+    type: String,
+    default: '0 0 128 128'
+  }
+});
+</script>
